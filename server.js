8a4b17512ccfcc0a6bac6e7ffa449c34993efa21
foropo05 <foropo@my.centennialcollege.ca>
2026-04-01 15:06:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 
 app.use(express.json());
 
