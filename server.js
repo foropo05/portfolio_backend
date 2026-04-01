d62b0dcc5054620a05ad623ada564f29e6028e45
foropo05 <foropo@my.centennialcollege.ca>
2026-04-01 14:10:24 -0400

    Backend API completed with full CRUD and CORS setup

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const connectDB = require("./config/db");
 const referenceRoutes = require("./routes/reference.routes");
 const projectRoutes = require("./routes/project.routes");
 const serviceRoutes = require("./routes/service.routes");
 const userRoutes = require("./routes/user.routes");
+
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
 app.use(express.json());
 
-// optional test home route
 app.get("/", (req, res) => {
   res.send("✅ API is running");
 });
 
-// mount routes
 app.use("/api/references", referenceRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/services", serviceRoutes);
@@ -25,4 +30,3 @@ const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 });
-
